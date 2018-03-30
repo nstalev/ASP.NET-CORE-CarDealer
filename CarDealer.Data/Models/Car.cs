3c45e674ededf13719189c4fdd8edd32ea6918be
nstalev <stalev_a@abv.bg>
2018-03-30 11:43:07 +0300

    Project Start

---
+++ CarDealer.Data/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarDealer.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        public List<Sale> Sales { get; set; } = new List<Sale>();
+
+        public List<PartCar> Parts { get; set; } = new List<PartCar>();
+    }
+}
---
+++ CarDealer.Data/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarDealer.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsYoungDriver { get; set; }
+
+        public List<Sale> Sales { get; set; } = new List<Sale>();
+
+    }
+}
